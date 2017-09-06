ba69f959e010804097d3e325acffe06ca71a4612
vitaliy-zinchenko <zinjvi@gmail.com>
2017-09-06 22:00:37 +0300

    merge sort

--- try-mvn/src/main/java/zinjvi/algo/Sort.java
+++ try-mvn/src/main/java/zinjvi/algo/Sort.java
@@ -1,5 +1,7 @@
 package zinjvi.algo;
 
+import java.util.Arrays;
+
 /**
  * @author Vitalii Zinchenko
  */
@@ -43,4 +45,47 @@ public class Sort extends Loggable {
         array[j] = temp;
     }
 
+    public static <T> void mergeSort(T[] array, Comparator<T> comparator) {
+        int left = 0;
+        int right = array.length - 1;
+        doMergeSort(array, comparator, left, right);
+    }
+
+    private static <T> void doMergeSort(T[] array, Comparator<T> comparator, int left, int right) {
+        log("doMergeSort(%s, %d, %d)", Arrays.toString(array), left, right);
+        if(left == right) { // TODO what is better?
+            return;
+        }
+        int center = (left + right) / 2;
+        doMergeSort(array, comparator, left, center);
+        doMergeSort(array, comparator, center + 1, right);
+        merge(array, comparator, left, center, right);
+    }
+
+    public static <T> void merge(T[] array, Comparator<T> comparator, int left, int center, int right) {
+        T[] leftArray = createArray(array, left, center - left + 1);
+        T[] rightArray = createArray(array, center + 1, right - center);
+        for(int i = left, leftPos = 0, rightPos = 0; i <= right ; i++) {
+            if(leftPos == leftArray.length) { // TODO refactor
+                array[i] = rightArray[rightPos];
+                rightPos++;
+            } else if(rightPos == rightArray.length) {
+                array[i] = leftArray[leftPos];
+                leftPos++;
+            } else if(comparator.compare(leftArray[leftPos], rightArray[rightPos]) < 0) {
+                array[i] = leftArray[leftPos];
+                leftPos++;
+            } else {
+                array[i] = rightArray[rightPos];
+                rightPos++;
+            }
+        }
+        log("merged: %s, %d, %d, %d", Arrays.toString(array), left, center, right);
+    }
+
+    private static <T> T[] createArray(T[] src, int srcStart, int length) {
+        T[] array = (T[]) new Object[length];
+        System.arraycopy(src, srcStart, array, 0, length);
+        return array;
+    }
 }
--- try-mvn/src/test/java/zinjvi/algo/SortTest.java
+++ try-mvn/src/test/java/zinjvi/algo/SortTest.java
@@ -52,4 +52,32 @@ public class SortTest {
         Assert.assertArrayEquals(new Integer[]{1, 2, 3, 5, 6, 7}, array);
     }
 
+    @Test
+    public void mergeTest() {
+        Integer[] array = new Integer[]{1, 3, 5, 2, 4, 6};
+        Sort.merge(array, INTEGER_COMPARATOR, 0, 2, 5);
+        Assert.assertArrayEquals(new Integer[]{1, 2, 3, 4, 5, 6}, array);
+    }
+
+    @Test
+    public void mergeTest2() {
+        Integer[] array = new Integer[]{1, 2, 3, 4, 5, 6};
+        Sort.merge(array, INTEGER_COMPARATOR, 0, 2, 5);
+        Assert.assertArrayEquals(new Integer[]{1, 2, 3, 4, 5, 6}, array);
+    }
+
+    @Test
+    public void mergeTest3() {
+        Integer[] array = new Integer[]{4, 5, 6, 1, 2, 3};
+        Sort.merge(array, INTEGER_COMPARATOR, 0, 2, 5);
+        Assert.assertArrayEquals(new Integer[]{1, 2, 3, 4, 5, 6}, array);
+    }
+
+    @Test
+    public void mergeSortTest() {
+        Integer[] array = new Integer[]{3, 2, 5, 1, 6, 4};
+        Sort.mergeSort(array, INTEGER_COMPARATOR);
+        Assert.assertArrayEquals(new Integer[]{1, 2, 3, 4, 5, 6}, array);
+    }
+
 }
